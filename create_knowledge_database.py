f4b7200d4b1bdcfb651bddaa675d793b9af9df5c
vpooz <vova0306pozdeev@yandex.ru>
2026-04-16 12:28:05 +0300

    feat(chunking): add function to create index in faiss knowledgebase

--- create_knowledge_database.py
+++ create_knowledge_database.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 from TextSplitter import chunk_text
 from tqdm import tqdm
+import faiss
 model = SentenceTransformer('sentence-transformers/multi-qa-mpnet-base-dot-v1')
 def create_embed(path_to_file:str,
                  path_file_to_save_text_chunks:str,path_file_to_save_embed_chunks:str,
@@ -28,21 +29,31 @@ def create_embed(path_to_file:str,
     np.save(path_file_to_save_text_chunks,chunks)
     np.save(path_file_to_save_embed_chunks, embed_chunks)
 
+def build_index(path_to_embedded_chunks:str,path_to_save_index:str):
+    embed_chunks=np.load(path_to_embedded_chunks)
+    index=faiss.IndexFlatIP(embed_chunks.shape[1])
+    index.add(embed_chunks)
+    faiss.write_index(index, path_to_save_index)
 if __name__ == '__main__':
-    create_embed('parsed_pages/hunter_parsed_pages.jsonl',
+    '''create_embed('parsed_pages/hunter_parsed_pages.jsonl',
                  'hunter_chunks.npy',
-                 'hunter_embedding_chunks.npy')
+                 'hunter_embedding_chunks.npy',source='hunter')
     arr1=np.load('hunter_embedding_chunks.npy')
     arr2=np.load('hunter_chunks.npy',allow_pickle=True)
-
+    print(len(arr1),len(arr2))
 
     create_embed('parsed_pages/naruto_parsed_pages.jsonl',
                  'naruto_chunks.npy',
-                 'naruto_embedding_chunks.npy')
+                 'naruto_embedding_chunks.npy',source='naruto')
 
     create_embed('parsed_pages/sao_parsed_pages.jsonl',
                  'sao_chunks.npy',
-                 'sao_embedding_chunks.npy')
+                 'sao_embedding_chunks.npy',source='sao')'''
+
+    build_index('chunks/hunter_embedding_chunks.npy', 'hunter.faiss')
+    build_index('chunks/naruto_embedding_chunks.npy', 'naruto.faiss')
+    build_index('chunks/sao_embedding_chunks.npy', 'sao.faiss')
+
 
 
 
